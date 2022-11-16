20125d9b4735d177ec77f3ba5149873cf34be439
Serene <ser3ene@gmail.com>
2022-11-16 18:52:48 +0800

    commit 2nd try

--- BankAccount/src/main/java/BankAccount.java
+++ BankAccount/src/main/java/BankAccount.java
@@ -1,79 +1,85 @@
 import java.time.LocalDate;
 
-import static java.lang.Double.sum;
 import static java.lang.Math.multiplyExact;
-import static javax.swing.Spring.minus;
 
 public class BankAccount {
 
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
-    private String accountNumber;
+    private Integer accountNumber;
     private Double balance;
+    private String accounttype;
+    private Double overdraft;
 
-    public BankAccount(String firstName, String lastName, LocalDate dateOfBirth, String accountNumber, Double balance){
+    public BankAccount(
+            String firstName,
+            String lastName,
+            LocalDate dateOfBirth,
+            Integer accountNumber,
+            String accounttype,
+            Double overdraft){
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
         this.accountNumber = accountNumber;
-        this.balance = balance;
+        this.balance = 0.00; //starting is 0!!!
+        this.accounttype = accounttype;
+        this.overdraft = overdraft;
 
     }
 
-    public void deposit(Double depositAmount) {
-        return "Successfully deposited. Your new balance is: " + sum(balance + depositAmount);
+    public void deposit(Double depositAmount) { //public/private Outcome Method // deposit(double) -> string
+        this.balance += depositAmount;
     }
 
-    public void withdrawal(Double withdrawalAmount) {
-        return "Successfully withdrawn. Your new balance is: " + minus(balance - withdrawalAmount);
+    public void withdraw(Double withdrawalAmount) {
+        if ((this.balance - withdrawalAmount) >= overdraft){
+            this.balance -= withdrawalAmount;
+        }
     }
 
-    public void interest(Double interestPercentage) {
-        return "Interest added: " + multiplyExact(balance * 1.05);
+    public Double calculateInterest(Double interestPercentage) {
+        Double interestToPay = this.balance * interestPercentage;
+        return interestToPay;
     }
 
+    public void payInterest(Double savingsAccInterest, Double currentAccInterest) {
+        if (accounttype.equals("savings")) {
+            this.balance += calculateInterest(savingsAccInterest);
+        } else if (accounttype.equals("current")) {
+            this.balance += calculateInterest((currentAccInterest));
+        }
+    }
     //GETTERS AND SETTERS
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getFirstName() { return firstName; }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public void setFirstName(String firstName) {this.firstName = firstName;}
 
-    public String getLastName() {
-        return lastName;
-    }
+    public String getLastName() {return lastName;}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public void setLastName(String lastName) {this.lastName = lastName;}
 
-    public LocalDate getdateofBirth() {
-        return dateOfBirth;
-    }
+    public LocalDate getdateofBirth() {return dateOfBirth;}
 
-    public void setdateofBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
+    public void setdateofBirth(LocalDate dateOfBirth) {this.dateOfBirth = dateOfBirth;}
 
-    public String getAccountNumber() {
-        return accountNumber;
-    }
+    public Integer getAccountNumber() {return accountNumber;}
 
-    public void setAccountNumber(String accountNumber){
-        this.accountNumber = accountNumber;
-    }
+    public void setAccountNumber(Integer accountNumber){this.accountNumber = accountNumber;}
 
-    public String getBalance() {
-        return balance;
-    }
+    public Double getBalance() {return balance;}
 
-    public void setBalance(Double balance) {
-        this.balance = balance;
-    }
+    public void setBalance(Double balance) {this.balance = balance;}
+
+        public String accounttype() {return accounttype;}
+
+        public void setAccountType(String accounttype){this.accounttype = accounttype;}
+
+        public Double overdraft() {return overdraft;}
+
+        public void setoverdraft (Double overdraft) {this.overdraft = overdraft;}
 
 
 }
--- BankAccount/src/test/java/BankAccountTest.java
+++ BankAccount/src/test/java/BankAccountTest.java
@@ -8,41 +8,39 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BankAccountTest {
 
-    private BankAccount bankAccount;
+    BankAccount bankAccount;
 
     @BeforeEach
     public void setUp() {
-        bankAccount = new BankAccount("Serene", "Chan",
-                LocalDate.of(2000, 9, 29), 1);
+        bankAccount = new BankAccount(
+                "Serene",
+                "Chan",
+                LocalDate.of(2000, 9, 29),
+                064,
+                "current",
+                -50.00);
     }
 //set balance as 0
 
     @Test
-    public void testDeposit() {
-        String expected = "Successfully deposited. Your new balance is: 100.00";
-        String actual = BankAccount.deposit(100.00);
-        assertThat(bankAccount.deposit(100);)
-
-        expected = "Successfully deposited. Your new balance is: 99.00";
-        actual = bankAccount.deposit(199.00);
-        System.out.println(actual);
-        assertThat(bankAccount.deposit(199.00);)
+    public void canDeposit() {
+        bankAccount.deposit(100.0);
+        assertThat(bankAccount.getBalance()).isEqualTo(100.0);
     }
 
     @Test
-    public void testWithdrawl(){
-        String expected = "Successfully withdrawn. Your new balance is: 99.00";
-        String actual = BankAccount.withdrawal(1.00);
-        assertThat(bankAccount.withdrawal(99.00);)
-
-        String expected = "Successfully withdrawn. Your new balance is: 98.00";
-        String actual = bankAccount.withdrawal(2.00);
-        assertThat(bankAccount.withdrawal(98.00);)
+    public void canWithdraw() {
+        bankAccount.deposit(100.0);
+        bankAccount.withdraw(1.0);
+        assertThat(bankAccount.getBalance()).isEqualTo(99.0);
     }
 
     @Test
-    //public void testInterest(){
-       // String expected = "Interest added: " + //balance * 1.05
-    }
+    public void canPayInterest() {
+        bankAccount.deposit(100.0);
+        bankAccount.payInterest(0.02, 0.01);
+        assertThat(bankAccount.getBalance()).isEqualTo(101.0);
 
+    }
+//TEST FOR GETTERS AND SETTERS
 }
